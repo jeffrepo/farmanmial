5c4850f111fb0d6f5a3935ad22042283e702f31a
jeffrepo <jeffer.leo93@gmail.com>
2025-06-10 07:33:11 -0600

    Mosrar cantidad de productos

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Farm animals",
+    'summary': """
+        Desarrollos para Farm animals""",
+    'description': """
+       Desarrollos para Farm animalas
+    """,
+    'author': "ST",
+    'website': "",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['stock'],
+    'data': [
+        'report/report_picking.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'aplication' : False,
+}
